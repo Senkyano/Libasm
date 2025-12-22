885b67a63854fddb059218e3d7687db6b55f30fa
Rithy-Yano Hoy <rihoy@f1r4s15.clusters.42paris.fr>
2025-12-22 19:26:32 +0100

    ft_list_sort start

--- ft_list_sort.asm
+++ ft_list_sort.asm
@@ -14,4 +14,23 @@ section .text
 	global	ft_list_sort
 
 	ft_list_sort:
+		test	rdi,	rdi
+		jz		.error
+
+		push	rbx;
+		push	r12;
+		push	r13
+		push	r14
+		push	r15
+
+
+	.pop_done:
+		pop		r15
+		pop		r14
+		pop		r13
+		pop		r12
+		pop		rbx
+
+	.done:
+		ret
 		
