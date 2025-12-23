c029312dba5af9fa731a3402ca26a85b713bb3ec
Rithy-Yano Hoy <rihoy@f2r8s7.clusters.42paris.fr>
2025-12-23 17:05:59 +0100

    null

--- ft_list_remove_if.asm
+++ ft_list_remove_if.asm
@@ -1,20 +1,52 @@
 
 bits 64
 
-;| Registre ;| Usage                                                        |
-;| -------- ;| ------------------------------------------------------------ |
-;| `rax`    ;| if we need to remove and at the end we return rax 			|
-													;to point first element |
-;| `rdi`    ;| actual data                                                  |
-;| `r9`     ;| function to call                                             |
-;| `r8`     ;| first data of list                                           |
-
 section .text
 	global	ft_list_remove_if
+	extern	free
 
 	ft_list_remove_if:
+		test	rdi,	rdi
+		jz		.done
+		test	rdx,	rdx
+		jz		.done
+		test	rcx,	rcx
+		jz		.done
 
+		push	rbx
+		push	r12
+		push	r13
+		push	r14
+		push	r15
 
-	.loop_condition:
+		mov		rbx,	rdi;	begin list
+		mov		r12,	rsi;	data ref
+		mov		r13,	rdx;	function cmp
+		mov		r14,	rcx;	free_fct
 
-		jmp	.loop_condition
+	.loop_to_end:
+		mov		r15,	[rbx]
+		test	r15,	r15;	test la node
+		jz		.pop_done
+
+		mov		rdi,	[r15];	data
+		mov		rsi,	r12;	function call
+		call	r13
+		test	eax,	eax
+		jnz		.skip_node
+
+	.skip_node:
+		
+		jmp		.loop_to_end
+
+	.pop_done:
+		pop		r15
+		pop		r14
+		pop		r13
+		pop		r12
+		pop		rbx
+
+	.done:
+		ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
