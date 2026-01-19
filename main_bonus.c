e4e2ec64e28304cb86dc93a8af019d62c870fd00
Rithy-Yano <rithyyano.hoy@gmail.com>
2026-01-19 18:38:52 +0100

    add test list_size && push_front

--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 23:29:21 by rihoy             #+#    #+#             */
-/*   Updated: 2026/01/18 15:38:38 by rihoy            ###   ########.fr       */
+/*   Updated: 2026/01/19 18:36:37 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,69 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 #include "libasm.h"
 
 int main(void) {
 	printf("TEST ATOI\n");
 	
+	printf(RED"\n Test null string\n\n"RST);
 	printf(CY"\nresult = %d"RST",	Base = 01, number = NULL\n", ft_atoi_base(NULL, "01"));
 	printf(GR"expected result = 0\n"RST);
 	printf(CY"\nresult = %d"RST", 	Base = NULL, number = 11\n", ft_atoi_base("11", NULL));
 	printf(GR"expected result = 0\n"RST);
-	
-	printf("\n\nTEST LIST_PUSH_FRONT\n");
+	printf(CY"\nresult = %d"RST", 	Base = NULL, number = NULL\n", ft_atoi_base(NULL, NULL));
+	printf(GR"expected result = 0\n"RST);
+	printf("\n Test whitespace string\n");	
+	printf(CY"\nresult = %d"RST",	Base = Decimal, number = '  	14'\n", ft_atoi_base("  	14", "0123456789"));
+	printf(GR"expected result = 14\n"RST);
+	printf(CY"\nresult = %d"RST", 	Base = Decimal, number = ' 11   12'\n", ft_atoi_base(" 11   12", "0123456789"));
+	printf(GR"expected result = 11\n"RST);
+	printf(RED"\n Test duplicate character\n"RST);	
+	printf(CY"\nresult = %d"RST", 	Base = 112, number = ' 11   12'\n", ft_atoi_base(" 11   12", "112"));
+	printf(GR"expected result = 0\n"RST);
 
-	printf("\n\nTEST LIST_SIZE\n");
+	// 11 base 01
+	printf("\n Test normal string\n");
+	printf(CY"\nresult = %d"RST", 	Base = 01, number = 11\n", ft_atoi_base("11", "01"));
+	printf(GR"expected result = 3\n"RST);
+	printf(CY"\nresult = %d"RST", 	Base = 01, number = 1111111111111111111111111111111\n", ft_atoi_base("1111111111111111111111111111111","01"));
+	printf(GR"expected result = 2147483647\n"RST);
+	printf(CY"\nresult = %d"RST", 	Base = 01, number = -10000000000000000000000000000000\n", ft_atoi_base("-10000000000000000000000000000000","01"));
+	printf(GR"expected result = -2147483648\n"RST);
+	printf(CY"\nresult = %d"RST", 	Base = Decimal, number = -2147483648\n", ft_atoi_base("-2147483648","0123456789"));
+	printf(GR"expected result = -2147483648\n"RST);
+	printf(CY"\nresult = %d"RST", 	Base = Decimal, number = 21474836487\n", ft_atoi_base("2147483647","0123456789"));
+	printf(GR"expected result = 2147483647\n"RST);
+	printf(RED"INT_MAX + 1\n"RST);
+	printf(CY"result = %d"GR", expected result = %d\n"RST, ft_atoi_base("21474836488", "0123456789"), atoi("21474836488"));
+	printf(RED"INT_MIN - 1\n"RST);
+	printf(CY"result = %d"GR", expected result = %d\n"RST, ft_atoi_base("-21474836489", "0123456789"), atoi("-21474836489"));
+	
+	printf("\n\nTEST LIST_SIZE && TEST LIST_PUSH_FRONT\n");
+
+
+	t_list	*list_p;
+	list_p = malloc(sizeof(t_list));
+	list_p->data = NULL;
+	list_p->next = NULL;
+	
+	printf(CY"result size = %d"GR" expected = 1\n"RST,ft_list_size(list_p));
+	printf("%p\n", list_p->data);
+	for (int i = 0; i < 10; i++) {
+		if (i % 2 == 1)
+			ft_list_push_front(&list_p, &i);
+		else
+			ft_list_push_front(&list_p, NULL);
+	}
+	printf(CY"result size = %d"GR" expected = 11\n"RST,ft_list_size(list_p));
+	t_list	*tmp2;
+	while (list_p) {
+		tmp2 = list_p;
+		printf("%p\n", tmp2->data);
+		list_p = list_p->next;
+		free(tmp2);
+	}
 
 	return (1);
 }
